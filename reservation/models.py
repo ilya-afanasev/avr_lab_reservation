14fc2e81c00b9dddf65a55968ab554dd2c9b74f7
Ilya Afanasev <ilya.orsk@gmail.com>
2016-11-28 18:31:16 +0300

    Add __str__ methods for models, remove first_name and last_name from User model

--- reservation/models.py
+++ reservation/models.py
@@ -10,6 +10,9 @@ class Resource(db.Model):
     type = db.Column(db.Integer, db.ForeignKey('resource_types.id'))
     reservation = db.relationship('Reservation', backref='Resource')
 
+    def __str__(self):
+        return str(self.name)
+
 
 class ResourceType(db.Model):
     __tablename__ = 'resource_types'
@@ -17,6 +20,24 @@ class ResourceType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     resource = db.relationship('Resource', backref='ResourceType')
+
+    def __str__(self):
+        return str(self.name)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    email = db.Column(db.String(255), unique=True)
+    github_id = db.Column(db.Integer(), unique=True)
+
+    reservation = db.relationship('Reservation', backref='User')
+
+    def __str__(self):
+        return "{} {}, ID: {}, Email: {} github_id: {}".format(
+            self.first_name, self.last_name, self.id, self.email, self.github_id)
 
 
 class Reservation(db.Model):
@@ -32,13 +53,6 @@ class Reservation(db.Model):
 
     db.CheckConstraint('start_datetime < end_datetime', name='dates_check')
 
-
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-
-    email = db.Column(db.String(255), unique=True)
-    github_id = db.Column(db.Integer(), unique=True)
+    def __str__(self):
+        return "Resource id {}, user_id {}, start {}, end {}".format(
+            self.resource_id, self.user_id, self.start_datetime, self.end_datetime)
