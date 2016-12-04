5cb902a22bf8b587fe71860e5c4c744b09fc5473
Ilya Afanasev <ilya.orsk@gmail.com>
2016-12-04 03:19:11 +0300

    Add more validators for admin views

--- reservation/admin_views.py
+++ reservation/admin_views.py
@@ -1,25 +1,36 @@
 from flask_admin.contrib.sqla import ModelView
 from wtforms import validators
 
-from reservation.validators import IsReserved, StartLessEnd
+from reservation.validators import IsReserved, StartLessEnd, OneOfRequired
 
 
 class UserAdminView(ModelView):
     form_excluded_columns = ['reservation']
     form_args = {
-        'email': {'validators': [validators.email()]}
+        'email': {'validators': [validators.email(), OneOfRequired('email', 'github_id')]}
     }
 
 
 class ResourceAdminView(ModelView):
     form_excluded_columns = ['reservation']
+    form_args = {
+        'name': {'validators': [validators.required()]},
+        'ResourceType': {'label': 'Resource type', 'validators': [validators.required()]}
+    }
 
 
 class ReservationAdminView(ModelView):
     form_args = {
-        'end_datetime': {'validators': [IsReserved(), StartLessEnd()]}
+        'end_datetime': {'validators': [validators.required(), IsReserved(), StartLessEnd()]},
+        'start_datetime': {'validators': [validators.required()]},
+        'token': {'validators': [validators.required()]},
+        'Resource': {'validators': [validators.required()]},
+        'User': {'validators': [validators.required()]}
     }
 
 
 class ResourceTypeAdminView(ModelView):
     form_excluded_columns = ['resource']
+    form_args = {
+        'name': {'validators': [validators.required()]}
+    }
