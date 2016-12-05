0475a5cbf5aa90b444aba743fdbb8972164c2966
Ilya Afanasev <ilya.orsk@gmail.com>
2016-12-05 16:40:55 +0300

    Add env variables for PORT

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from reservation import app
 
 if __name__ == '__main__':
-    port = 5555
-    host = '127.0.0.1'
+    port = int(os.environ.get('PORT', 5555))
+    host = '0.0.0.0'
     debug = os.environ.get('DEBUG', 'True') == 'True'
     app.run(host=host, port=port, debug=debug)
